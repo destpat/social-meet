849ea95fab52259753d6c1e6987859b1ea0adf07
Yoan Ficadiere <yficadiere@inbox.fr>
2017-09-02 14:14:29 +0200

    Adding property key inside date object

--- src/scenes/NewUser.js
+++ src/scenes/NewUser.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 
 export default class NewUser extends React.Component {
   render() {
     return (
       <View>
         <Text>NewUser</Text>
+        <FlatList
+          data={[{key: 'a'}, {key: 'b'}]}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+        />
       </View>
     );
   }
